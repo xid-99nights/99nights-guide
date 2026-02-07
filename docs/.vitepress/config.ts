803d2bbbe97d5ed1ade2e004f4d949d8300348aa
Xid <xid.wores@gmail.com>
2026-02-06 17:41:25 -0700

    VitePress config + nav/sidebar + index home

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,25 @@
 import { defineConfig } from 'vitepress'
 
-// https://vitepress.vuejs.org/config/app-configs
-export default defineConfig({})
+export default defineConfig({
+  title: '99 Nights in the Forest Guide',
+  description: 'Player wiki mirror',
+  themeConfig: {
+    nav: [
+      { text: 'Enemies', link: '/enemies' },
+      { text: 'Items', link: '/items' },
+      { text: 'Classes', link: '/classes' },
+      { text: 'NPCs', link: '/npcs' },
+    ],
+    sidebar: [
+      {
+        text: 'Wiki',
+        items: [
+          { text: 'Enemies', link: '/enemies' },
+          { text: 'Items', link: '/items' },
+          { text: 'Classes', link: '/classes' },
+          { text: 'NPCs', link: '/npcs' },
+        ]
+      }
+    ]
+  }
+})
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,7 @@
-# Hello World
+---
+title: Home
+---
+
+# 99 Nights in the Forest - Player's Guide
+
+Wiki mirror: Enemies, Items, Classes, NPCs.
